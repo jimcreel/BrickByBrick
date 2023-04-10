c25532660bccf1acd0bb2199e6b8c78a46861362
jimcreel <jim.creel@gmail.com>
2023-04-10 00:53:37 -0700

    moving static

--- brickbybrick/settings.py
+++ brickbybrick/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
