fea44e0f4cb57fa7dcfa982cfa623a4472ab458f
jimcreel <jim.creel@gmail.com>
2023-08-07 13:15:36 -0700

    re-styled card grid

--- brickbybrick/settings.py
+++ brickbybrick/settings.py
@@ -15,7 +15,8 @@ from pathlib import Path
 
 import os, django_on_heroku
 
-
+from dotenv import load_dotenv
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -24,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
